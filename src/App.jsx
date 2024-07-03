c8f6dd320bca43a9d24ff8df24199a352377a577
vineeth <vineethkumar0005@gmail.com>
2024-07-04 01:49:55 +0530

    Fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import componentsImg from './assets/components.png';
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcepts.jsx';
 import {CORE_CONCEPTS} from './data.js'
+import {EXAMPLES} from './data.js'
+import TabButton from './components/TabButton.jsx';
+import {useState} from 'react';
+
 
 function App() {
+  const [tabContent,setTabContent] = useState();
+  
+  function clickHandler(buttonName) {
+    setTabContent(buttonName)
+  }
+  let tabInformation = <p>Please select the Tab!</p>
+  if(tabContent) {
+    tabInformation = <div>
+    <h3>{EXAMPLES[tabContent].title}</h3>
+    <p>{EXAMPLES[tabContent].description}</p>
+    <pre>
+    <code>{EXAMPLES[tabContent].code}</code>
+    </pre>
+    </div>
+  }
   return (
     <div>
       <Header />
@@ -23,6 +42,19 @@ function App() {
         </section>
         <h2>Time to get started!</h2>
         <h2>This page is created just to publish in Github as git Pages</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => clickHandler('components')}>Components</TabButton>
+            <TabButton onSelect={() => clickHandler('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => clickHandler('props')}>Props</TabButton>
+            <TabButton onSelect={() => clickHandler('state')}>State</TabButton>
+          </menu>
+        </section>
+        <div id="tab-content">
+          {tabInformation}
+        </div>
+        
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton({children,onSelect}) {
+    return(
+        <button onClick={onSelect}>{children}</button>
+    );
+}
